2f7196a0db45b5840f4368cf67ea96d67c5f9c4a
yoguang <wygdancer@aliyun.com>
2021-04-08 22:29:36 +0800

    feat: 新增 posts 博客接口

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsController } from './posts/posts.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, PostsController], // 新增 RostsController 博客接口
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,57 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+
+const PostsListMock = [
+  { id: '1', title: '我是帖子1', author: 'stephen_w' },
+  { id: '2', title: '我是帖子2', author: 'stephen_y' },
+  { id: '3', title: '我是帖子3', author: 'stepehn_g' },
+];
+
+interface ICreateBody {
+  title: string;
+  author: string;
+}
+
+interface IResponse {
+  success: boolean;
+  result: any;
+}
+
+@Controller('posts')
+export class PostsController {
+
+  @Get('list')
+  getList(@Query() query): IResponse { // 使用 Query 装饰器获取接口？之后的参数
+    console.log('query----->', query)
+    return {
+      success: true,
+      result: PostsListMock,
+    };
+  }
+
+  @Post('create')
+  create(@Body() body: ICreateBody): IResponse { // 使用 Body 装饰器获取 body 中的数据
+    const { title, author } = body;
+    console.log('title, author---->', body);
+    const createId = Date.now().toString();
+    return {
+      success: true,
+      result: createId,
+    };
+  }
+
+  @Get(':id')
+  detail(@Param('id') id: string): IResponse { // 使用 @Parma 装饰器获取接口参数帖子 ID
+    const post = PostsListMock.find(item => item.id === id);
+    if (post) {
+      return {
+        success: true,
+        result: post,
+      };
+    } else {
+      return {
+        success: false,
+        result: null,
+      }
+    }
+  }
+}
